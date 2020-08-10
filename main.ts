a324301267310dd4789c081615921b55b38ea96b
C-VR <69105079+C-VR@users.noreply.github.com>
2020-08-10 10:14:39 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,29 @@
 radio.onReceivedString(function (receivedString) {
-    serial.writeLine(receivedString)
+    bericht = receivedString
+    teller += 1
+    if (teller == 2) {
+        teller = 0
+        printTekst = "" + printTekst + bericht
+        serial.writeLine(printTekst)
+        printTekst = ""
+    } else if (teller == 1) {
+        printTekst = "" + printTekst + bericht
+    }
 })
-radio.setGroup(255)
+let printTekst = ""
+let bericht = ""
+let teller = 0
 serial.redirect(
-SerialPin.P8,
 SerialPin.P1,
+SerialPin.P8,
 BaudRate.BaudRate19200
 )
 serial.writeLine("")
+radio.setGroup(255)
+teller = 0
+bericht = ""
+printTekst = ""
+basic.forever(function () {
+    led.toggle(2, 2)
+    basic.pause(1000)
+})
